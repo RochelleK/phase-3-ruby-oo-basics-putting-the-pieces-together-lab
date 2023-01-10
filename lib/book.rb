48a2fa3bb089bf350e59f846f4536f8165340e12
Rochelle Katzman <zelrochelle@gmail.com>
2023-01-10 15:28:39 -0500

    commit tree e0ff253e5ada378aed148388aa12702cc6215d39

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
 
+class  Book 
+    attr_accessor :book 
+
+def initialize(book, title, author, page_count)
+@book = book 
+@title = title 
+@author = author 
+@page_count = page_count
+@genre = genre
+end 
+    
+title author genre
+
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+
+end 
+
+end
